b30b80933d69484c2811162430a5d97f922fa649
Tobias Beese <tobias.beese@gmx.de>
2023-08-18 21:53:09 +0200

    Add `Link` component

---
+++ web/src/components/Link/Link.tsx
@@ -0,0 +1,14 @@
+import { ComponentProps } from 'react'
+
+import { Link as RedwoodLink } from '@redwoodjs/router'
+
+const Link = (props: ComponentProps<typeof RedwoodLink>) => {
+  return (
+    <RedwoodLink
+      className="rounded-lg bg-teal-200 px-6 py-3 text-2xl shadow-md hover:bg-teal-300 active:translate-y-1 md:px-4 md:py-2 md:text-base "
+      {...props}
+    />
+  )
+}
+
+export default Link
