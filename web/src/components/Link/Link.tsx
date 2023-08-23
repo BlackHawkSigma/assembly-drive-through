834817580b9cc2eddefd7583751e02a98cf0f8a8
Tobias Beese <tobias.beese@gmx.de>
2023-08-23 17:00:02 +0200

    Grow `Link`s

--- web/src/components/Link/Link.tsx
+++ web/src/components/Link/Link.tsx
@@ -5,7 +5,7 @@ import { Link as RedwoodLink } from '@redwoodjs/router'
 const Link = (props: ComponentProps<typeof RedwoodLink>) => {
   return (
     <RedwoodLink
-      className="rounded-lg bg-teal-200 px-6 py-3 text-center text-2xl shadow-md hover:bg-teal-300 active:translate-y-1 md:px-4 md:py-2 md:text-base"
+      className="grow rounded-lg bg-teal-200 px-6 py-3 text-center text-2xl shadow-md hover:bg-teal-300 active:translate-y-1 md:px-4 md:py-2 md:text-base"
       {...props}
     />
   )
