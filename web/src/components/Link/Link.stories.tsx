73555146b703bf2d4cbc3ec56bfa3685be6f727e
Tobias Beese <tobias.beese@gmx.de>
2023-08-23 13:20:17 +0200

    Center text in `Link` component

--- web/src/components/Link/Link.stories.tsx
+++ web/src/components/Link/Link.stories.tsx
@@ -1,21 +1,12 @@
-// Pass props to your component by passing an `args` object to your story
-//
-// ```jsx
-// export const Primary: Story = {
-//  args: {
-//    propName: propValue
-//  }
-// }
-// ```
-//
-// See https://storybook.js.org/docs/react/writing-stories/args.
-
 import type { Meta, StoryObj } from '@storybook/react'
 
 import Link from './Link'
 
 const meta: Meta<typeof Link> = {
   component: Link,
+  args: {
+    children: 'follow me',
+  },
 }
 
 export default meta
