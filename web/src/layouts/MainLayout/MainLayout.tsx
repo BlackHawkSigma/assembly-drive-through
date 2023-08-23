fc9604f2d502dd17f15fb3d50dfe5e9ae8c52dae
Tobias Beese <tobias.beese@gmx.de>
2023-08-23 13:31:19 +0200

    Update `MainLayout`

--- web/src/layouts/MainLayout/MainLayout.tsx
+++ web/src/layouts/MainLayout/MainLayout.tsx
@@ -9,12 +9,26 @@ const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <>
       <Toaster />
-      <nav className="shadow-teal-00 flex items-baseline justify-between bg-teal-400 p-2 shadow-md">
-        <Link className="text-2xl md:text-lg" to={routes.home()}>
-          Home
-        </Link>
-        <NavLink activeClassName="underline" to={routes.orders()}>
-          Anforderungen
+      <nav className="shadow-teal-00 flex items-baseline justify-end gap-4 bg-teal-400 p-2 shadow-md">
+        <div className="grow">
+          <Link className="text-2xl md:text-lg" to={routes.home()}>
+            Home
+          </Link>
+        </div>
+
+        <NavLink
+          className="rounded-md bg-black/10 px-4 py-1"
+          activeClassName="underline"
+          to={routes.newOrder()}
+        >
+          neuer Auftrag
+        </NavLink>
+        <NavLink
+          className="rounded-md bg-black/10 px-4 py-1"
+          activeClassName="underline"
+          to={routes.orders()}
+        >
+          Aufträge
         </NavLink>
       </nav>
 
