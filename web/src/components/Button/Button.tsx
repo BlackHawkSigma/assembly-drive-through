a81b3f4629373d751c5656c50e4d13802c61ab13
Tobias Beese <tobias.beese@gmx.de>
2023-08-25 12:15:57 +0200

    Style disabled `Button`

--- web/src/components/Button/Button.tsx
+++ web/src/components/Button/Button.tsx
@@ -3,7 +3,7 @@ import { ComponentProps } from 'react'
 const Button = ({ children, ...props }: ComponentProps<'button'>) => {
   return (
     <button
-      className="rounded-lg bg-teal-200 px-6 py-3 text-2xl shadow-md hover:bg-teal-300 active:translate-y-1 md:px-4 md:py-2 md:text-base "
+      className="rounded-lg bg-teal-200 px-6 py-3 text-2xl shadow-md hover:bg-teal-300 active:translate-y-1 disabled:pointer-events-none disabled:bg-slate-200 disabled:text-slate-700 md:px-4 md:py-2 md:text-base "
       {...props}
     >
       {children}
