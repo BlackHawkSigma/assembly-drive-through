c5581bd24bddc88cff6a1f2839b19bfeecba7d16
Tobias Beese <tobias.beese@gmx.de>
2023-09-05 11:22:29 +0200

    Wrap Links on `HomePage`

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -9,7 +9,7 @@ const HomePage = () => {
       <MetaTags title="Home" description="Startseite" />
       <div className="px-4">
         <h1 className="mb-4 py-2 text-center text-4xl">Startseite</h1>
-        <div className="flex flex-col justify-center gap-4 md:flex-row">
+        <div className="flex flex-col flex-wrap justify-center gap-4 md:flex-row">
           <div className="flex gap-4">
             <Link to={routes.newOrder()}>neuer Auftrag</Link>
             <Link to={routes.orders()}>Auftrags Liste</Link>
