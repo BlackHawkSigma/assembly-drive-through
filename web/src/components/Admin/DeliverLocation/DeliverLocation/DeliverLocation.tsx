a8989d13f2ffe589b3f364353a327acaf0ee0403
Tobias Beese <tobias.beese@gmx.de>
2024-02-14 12:39:19 +0100

    Lint imports

--- web/src/components/Admin/DeliverLocation/DeliverLocation/DeliverLocation.tsx
+++ web/src/components/Admin/DeliverLocation/DeliverLocation/DeliverLocation.tsx
@@ -1,13 +1,11 @@
-import { Link, routes, navigate } from '@redwoodjs/router'
-import { useMutation } from '@redwoodjs/web'
-import { toast } from '@redwoodjs/web/toast'
-
-import {} from 'src/lib/formatters'
-
 import type {
   DeleteDeliverLocationMutationVariables,
   FindDeliverLocationById,
 } from 'types/graphql'
+
+import { Link, routes, navigate } from '@redwoodjs/router'
+import { useMutation } from '@redwoodjs/web'
+import { toast } from '@redwoodjs/web/toast'
 
 const DELETE_DELIVER_LOCATION_MUTATION = gql`
   mutation DeleteDeliverLocationMutation($id: Int!) {
