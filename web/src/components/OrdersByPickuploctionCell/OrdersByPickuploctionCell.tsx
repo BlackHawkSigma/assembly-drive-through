e7cdd3eb53bc423839f960f96e986cce35551231
Tobias Beese <tobias.beese@opmobility.com>
2025-03-24 09:27:34 +0100

    Add polling to `OrdersByPickuploctionCell`

--- web/src/components/OrdersByPickuploctionCell/OrdersByPickuploctionCell.tsx
+++ web/src/components/OrdersByPickuploctionCell/OrdersByPickuploctionCell.tsx
@@ -29,6 +29,10 @@ export const QUERY = gql`
   }
 `
 
+export const beforeQuery = (props) => {
+  return { variables: props, pollInterval: 5_000 }
+}
+
 export const Loading = () => <div>Loading...</div>
 
 export const Empty = () => <CellEmpty />
